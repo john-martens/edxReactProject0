65f936a2ee6476d4632f05848c0226cb91bee0e8
John Martens <John.Martens@gmail.com>
2019-11-20 14:23:28 -0500

    Completed Project

--- script.js
+++ script.js
@@ -9,6 +9,38 @@ const list = document.getElementById('todo-list')
 const itemCountSpan = document.getElementById('item-count')
 const uncheckedCountSpan = document.getElementById('unchecked-count')
 
+function getCheckBox(name, label){
+    let checkHTML = "<input type='checkbox' class = '" + classNames.TODO_CHECKBOX + "' name = '" + name + "' id='" + name + "' onClick = toggleItem('" + name + "')>"
+    checkHTML += "<label for = '" + name + "'>" + label + "</label>"
+    return checkHTML
+}
+
+function toggleItem(item){
+  var checkBox = document.getElementById(item);
+  var label = checkBox.nextSibling;
+
+  label.classList.toggle("done");
+
+  if(checkBox.checked == true){
+      uncheckedCount--;
+  }
+  else{
+    uncheckedCount++;
+  }
+  uncheckedCountSpan.innerHTML = uncheckedCount.toString()
+
+
+}
+
+let itemCount=0
+let uncheckedCount=0
 function newTodo() {
-  alert('New TODO button clicked!')
+  let item = prompt("Enter a new todo item ")
+  if(item==null) return;
+  list.innerHTML += "<LI class='" + classNames.TODO_ITEM + "'><span class='" + classNames.TODO_TEXT + "'>" + getCheckBox("item" + itemCount, item) + "</SPAN></LI>"
+  itemCount++;
+  uncheckedCount++
+  itemCountSpan.innerHTML = itemCount.toString()
+  uncheckedCountSpan.innerHTML = uncheckedCount.toString()
+
 }
